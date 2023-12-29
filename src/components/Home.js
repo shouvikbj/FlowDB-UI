5d6066e222a9e207a159ad464720becce25e5000
Shouvik Bajpayee <shouvikbajpayee@Shouviks-MacBook-Air.local>
2023-12-29 11:27:12 +0530

    added loader for when the data is being loaded

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
   const [currentuser, setCurrentuser] = useState({});
   const [showModal, setShowModal] = useState(false);
   const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const openModal = () => {
     setShowModal(true);
@@ -18,6 +19,45 @@ const Home = () => {
 
   const closeModal = () => {
     setShowModal(false);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const form = document.getElementById("add-project-form");
+    await fetch(`${API}/create/project/${userid}`, {
+      method: "POST",
+      body: new FormData(form),
+      mode: "cors",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if (data.status === "ok") {
+          alert(data.message);
+          navigate(`/project/${data.projectid}`);
+        } else {
+          alert(data.message);
+        }
+      });
+    closeModal();
+  };
+
+  const getProjects = async (id) => {
+    await fetch(`${API}/get/projects/${id}`, {
+      method: "POST",
+      mode: "cors",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if (data.status === "ok") {
+          setProjects(data.projectnames);
+        } else {
+          alert(data.message);
+        }
+      });
   };
 
   const getUserDetails = async (id) => {
@@ -38,56 +78,27 @@ const Home = () => {
       });
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const form = document.getElementById("add-project-form")
-    await fetch(`${API}/create/project/${userid}`, {
-      method: "POST",
-      body: new FormData(form),
-      mode: "cors"
-    })
-    .then((response) => {
-      return response.json()
-    })
-    .then((data) => {
-      if(data.status === "ok"){
-        alert(data.message)
-        navigate(`/project/${data.projectid}`)
-      }
-      else{
-        alert(data.message)
-      }
-    })
-    closeModal();
-  };
-
-  const getProjects = async (id) => {
-    await fetch(`${API}/get/projects/${id}`, {
-      method: "POST",
-      mode: "cors"
-    })
-    .then((response) => {
-      return response.json()
-    })
-    .then((data) => {
-      if(data.status === "ok"){
-        setProjects(data.projectnames)
-      }
-      else{
-        alert(data.message)
-      }
-    })
-  }
-
   useEffect(() => {
     document.title = "FlowDB | Home";
-    if (Cookies.get("fdb-user")) {
-      setUserid(Cookies.get("fdb-user"));
-      getUserDetails(Cookies.get("fdb-user"));
-      getProjects(Cookies.get("fdb-user"))
-    } else {
-      navigate("/landingpage");
-    }
+
+    const fetchData = async () => {
+      try {
+        if (Cookies.get("fdb-user")) {
+          setUserid(Cookies.get("fdb-user"));
+          await getUserDetails(Cookies.get("fdb-user"));
+          await getProjects(Cookies.get("fdb-user"));
+          setLoading(false);
+        } else {
+          navigate("/landingpage");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false); // Set loading to false in case of an error
+      }
+    };
+
+    fetchData();
+
     return () => {
       document.title = "FlowDB";
     };
@@ -111,33 +122,45 @@ const Home = () => {
           </div>
         </header>
         <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-900 px-2 py-2">
-          <div className="container mx-auto mt-6 p-4 flex items-center justify-end">
-            <button
-              onClick={openModal}
-              className="bg-gray-800 text-white px-4 py-2 rounded-full hover:bg-gray-700"
-            >
-              Add Project
-            </button>
-          </div>
-          <div className="container mx-auto mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {projects.length === 0 ? (
-              <div className="flex items-center justify-center h-full">
-                <p className="text-center text-gray-300">No projects yet!</p>
-              </div>
-            ) : (
-              projects.map((project) => (
-                <Link
-                  key={project.projectid}
-                  to={`/project/${project.projectid}`}
-                  className="bg-gray-800 text-white rounded-2xl shadow p-4 hover:shadow-lg transition duration-300"
+          {loading ? (
+            <div className="flex items-center justify-center h-screen">
+              <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-300"></div>
+            </div>
+          ) : (
+            <>
+              <div className="container mx-auto mt-6 p-4 flex items-center justify-end">
+                <button
+                  onClick={openModal}
+                  className="bg-gray-800 text-white px-4 py-2 rounded-full hover:bg-gray-700"
                 >
-                  <h2 className="text-lg font-semibold mb-2">{project.projectname}</h2>
-                  <br/>
-                  <p className="text-gray">Click to view project details</p>
-                </Link>
-              ))
-            )}
-          </div>
+                  Add Project
+                </button>
+              </div>
+              <div className="container mx-auto mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {projects.length === 0 ? (
+                  <div className="flex items-center justify-center h-full">
+                    <p className="text-center text-gray-300">
+                      No projects yet!
+                    </p>
+                  </div>
+                ) : (
+                  projects.map((project) => (
+                    <Link
+                      key={project.projectid}
+                      to={`/project/${project.projectid}`}
+                      className="bg-gray-800 text-white rounded-2xl shadow p-4 hover:shadow-lg transition duration-300"
+                    >
+                      <h2 className="text-lg font-semibold mb-2">
+                        {project.projectname}
+                      </h2>
+                      <br />
+                      <p className="text-gray">Click to view project details</p>
+                    </Link>
+                  ))
+                )}
+              </div>
+            </>
+          )}
         </main>
       </div>
       {showModal && (
